d7128d2cba7c4a9b70482e2afd71a771e95e6421
Sir-Deon <sird655@gmail.com>
2020-03-20 19:59:12 -0700

    commiting

--- myapp/views.py
+++ myapp/views.py
@@ -5,6 +5,7 @@ from requests.compat import quote_plus
 from . import models
 
 BASE_URL = 'https://accra.craigslist.org/search/?query={}'
+BASE_URL_IMAGE ='https://images.craigslist.org/{}_300x300.jpg'
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
@@ -26,9 +27,20 @@ def new_search(request):
     for post in post_listings:
         post_title = post.find(class_='result-title').text
         post_url = post.find('a').get('href')
-        post_price = post.find(class_='result-price').text
 
-        final_post.append((post_title, post_url, post_price))
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'N/A'
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_URL_IMAGE.format(post_image_id)
+
+        else:
+            post_image_url = 'https://craigslist.org/images/peace.jpg'
+
+        final_post.append((post_title, post_url, post_price, post_image_url))
 
     suff_for_front_end = {
         'search': search,
